8913e96e991b7d57877a2587dbf6ab875d03223c
Krishna Sharma <148133555+Krishnasharma07@users.noreply.github.com>
2024-09-11 10:54:35 +0530

    Time: 3 ms (56.73%), Space: 11.6 MB (18.23%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+       unordered_map<int,int> m;
+
+       for(int i =0; i<arr.size(); i++)
+       {
+          m[arr[i]]++;
+       } 
+    set<int> s;
+       for(auto ele: m)
+       {
+        if(s.find(ele.second)!= s.end())
+        return false;
+        else
+        s.insert(ele.second);
+       }
+       return true;
+    }
+};
