be2ca0cb107a7fe6151e027bcf4420af32bae366
Krishna Sharma <148133555+Krishnasharma07@users.noreply.github.com>
2024-10-03 17:40:07 +0530

    Time: 74 ms (71.17%), Space: 70.6 MB (72.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,16 +5,15 @@ public:
         int cursum =0;
         for(int i =0; i<nums.size(); i++)
         {
-             if(cursum < 0)
-            {
-                cursum =0;
-            }
             cursum += nums[i];
             if(cursum > maxsum)
             {
                 maxsum = cursum;
             }
-           
+            if(cursum < 0)
+            {
+                cursum =0;
+            }
         }
         return maxsum;
     }
