6e3262eb484f77078f09bdbab147319ab3c08529
Krishna Sharma <148133555+Krishnasharma07@users.noreply.github.com>
2024-11-06 18:46:11 +0530

    Time: 0 ms (100%), Space: 7.5 MB (31.65%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -3,9 +3,13 @@ public:
     int fib(int n) {
         if(n == 0)
         return 0;
-        if( n == 1 || n == 2)
-        return 1;
-
-        return fib(n -  1) + fib(n-2);
+        int a=0; int b = 1;
+        for(int i =2 ; i <=n; i++)
+        {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 };
